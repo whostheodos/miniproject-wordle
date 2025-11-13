f89e890fcc562a498ce4fae5bf58da309cd056ef
whostheodos <theodospersonal@protonmail.com>
2025-11-13 16:04:46 +0100

    game logic

--- include/wordle_game.h
+++ include/wordle_game.h
@@ -8,6 +8,8 @@ void evaluate(const char* target,const char* input);
 void uppercase(char* str);
 const char* get_random_word();
 int is_valid_word(const char* word);
+int load_words(const char* file_name);
+int get_words_count();
 
 
 
--- src/wordle_game.c
+++ src/wordle_game.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include "../include/wordle_game.h"
 
+#include <stdlib.h>
+
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define GRAY "\033[0;90m"
+#define RESET "\033[0m"
+#define MAX_WORDS 100 //max to load
 
 void uppercase(char* str) {
     for (int i = 0; str[i];i++) {
@@ -16,4 +23,55 @@ int is_valid_word(const char* word) {
         if (!isalpha(word[i])) return 0;
     }
     return 1;
+}
+
+void evaluate(const char* target,const char* input) {
+    char result[word_length];
+    int target_count[26] = {0};
+    int used[word_length] = {0};
+
+    for (int i = 0; i < word_length; i++) {
+        if (input[i] == target[i]) {
+            result[i] = 'G';
+            used[i] = 1;
+        }else {
+            target_count[input[i] - 'A']++;
+        }
+    }
+    for (int i = 0; i < word_length; i++) {
+        if (result[i] == 'G') continue;
+        if (target_count[input[i] - 'A'] > 0) {
+            result[i] = 'Y';
+            target_count[input[i] - 'A']--;
+        }else {
+            result[i] = 'N';
+        }
+    }
+    for (int i = 0; i < word_length; i++) {
+        if (result[i] == 'G') {
+            printf("%s%c%s ",GREEN,input[i],RESET);
+        }else if (result[i] == 'Y') {
+            printf("%s%c%s ",YELLOW,input[i],RESET);
+        }else if (result[i] == 'N') {
+            printf("%s%c%s ",GRAY,input[i],RESET);
+        }
+    }
+    printf("\n");
+}
+
+void play() {
+    const char* target_word = get_random_word();
+    printf("------- WORDLE GAME -------\n");
+    printf("guess the 5-letter word. You have %d attempts.\n", MAX_ATTEMPTS);
+    printf("green = correct letter in correct position\n");
+    printf("yellow = correct letter in wrong position\n");
+    printf("gray = letter not in word\n\n");
+
+    char input[100];
+    int attempts = 0;
+    int won = 0;
+
+    while (attempts < 5 && !won) {
+        printf("");
+    }
 }
