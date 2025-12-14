87ef2bec9bb9afe58cfba97cd1499e48c5c6513e
thel1tch <jkinglow1962@gmail.com>
2025-12-14 14:36:15 +0100

    update to header so it matches .c file //still on progress

--- include/wordle_solver.h
+++ include/wordle_solver.h
@@ -10,30 +10,34 @@ typedef struct {
     int* positions; // position of the letter in the word
 } LetterFeedback;
 
-typedef struct  {
+typedef struct LetterFeedbackNode {
     LetterFeedback data;
-    struct LetterNode* next;
-} LetterNode;
+    struct LetterFeedbackNode* next;
+} LetterFeedbackNode;
 
 typedef struct {
-    LetterFeedback* head;
-    LetterFeedback* phaseHead;
+   LetterFeedbackNode* head;
 } Feedback;
 
-void solve(); // main solver function
-char* getbestguess (int guessNumber);//determine the suitable function to get the best guess
-char* getBestGuess1();//get the best first guess
-char* getBestGuess2(Feedback feedback, int guessNumber);//get the best guess based on feedback and guess number
+char** wordList; // Array of words loaded from words.txt
+int wordCount;  // Total number of words loaded
+int* letterFrequencies; // Array to hold letter frequencies
+
+
+
+
+char* getFirstguess();//get the best first guess
+void deleteNonMatchingWords(Feedback feedback,char** wordList);// delete words that do not match the feedback
+void loadWordsToRAM(FILE* src,char** wordList); // load words from words.txt to RAM
+char* getBestGuess(Feedback feedback, int guessNumber);//get the best guess based on feedback and guess number
 bool isLetterInWord(char letter, const char* word);// check if a letter is in a word
 bool isletterInPosition(char letter, int position, const char* word); // check if a letter is in a specific position in a word
 bool grayCompatible(const char* word, LetterFeedback Lfeedback); // check if a word is compatible with gray feedback
 bool yellowCompatible(const char* word, LetterFeedback Lfeedback); // check if a word is compatible with yellow feedback
 bool greenCompatible(const char* word, LetterFeedback Lfeedback); // check if a word is compatible with green feedback
-void keepOnlyInterphaseFeedback(Feedback* feedback);// clears old feedback and keep only interphase one
-FILE* doTempStats(FILE* file2 ); //creates a tempfile that has most common letters in words and their occururences
 char* pick (FILE* StatsFile); // picks the best word based on stats file
-void copyTXTFileContents(FILE* source, FILE* destination); // copy contents of one file to another
 void giveRatingToWords(FILE* source, FILE* destination); // give rating to words based on stats file
 bool IsWordMatchesFeedback(const char* word,Feedback feedback); // check if a word is valid by looking it up in words.txt
-
-#endif
+void cleanUpWordList(char** wordList); // shift the² words to remove NULLs
+void doStats(char** wordList ,int* letterFrequencies,int WordCount);// return an array of letter frequencies based on the current word list
+#endif 
