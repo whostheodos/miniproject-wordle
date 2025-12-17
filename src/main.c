5574121495d6a5834b59dd1d8b70cc600aa6da03
thel1tch <jkinglow1962@gmail.com>
2025-12-17 12:38:36 +0100

    Refactor input handling for game mode selection

--- src/main.c
+++ src/main.c
@@ -3,17 +3,19 @@
 #include "../include/wordle_solver.h"
 
 int main(){
-    int i;
-    printf("type 1 to play or 2 to solve");
-    scanf("%d",&i);
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-    if (i == 1) {
+    int mode;
+    printf("Type 1 to play or 2 to solve: ");
+    scanf("%d",&mode);
+    int c; while((c=getchar())!='\n' && c!=EOF);
+
+    if(mode == 1){
         play();
-
-    }else if (i == 2) {
-        solve_wordle();
-
+    } else if(mode == 2){
+        solve();
+    } else {
+        printf("Invalid option.\n");
     }
+
     return 0;
+}
 }
